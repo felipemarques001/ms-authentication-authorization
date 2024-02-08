db2c980f582192d594eaa5d4cf2a356d95e09645
Felipe Marques <felipemarquesgg@outlook.com>
2024-02-08 14:25:51 -0300

    Creating the project

---
+++ src/main/java/com/felipe/msauthenticationauthorization/MsAuthenticationAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.msauthenticationauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthenticationAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthenticationAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/msauthenticationauthorization/MsAuthenticationAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.msauthenticationauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthenticationAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
